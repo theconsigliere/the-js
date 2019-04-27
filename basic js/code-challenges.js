975b2d13989baba00c6cf4ae143a708f340d54d8
Maxwell Kirwin <maxwellkirwin@Maxwells-iMac.home>
2019-04-27 14:13:07 +0100

    section 2 completed

--- basic js/code-challenges.js
+++ basic js/code-challenges.js
@@ -100,3 +100,114 @@ if (mark.calcBmi() > john.calcBmi()) {
 } else {
   title.innerHTML = john.fullName + " has the same bmi as " + mark.fullName;
 }
+//
+//
+//
+//
+//
+//
+//
+//
+
+//coding challenge 5
+
+var john = {
+  fullName: "John Smith",
+  bills: [124, 48, 268, 180, 42],
+  calcTips: function() {
+    this.tips = [];
+    this.finalValues = [];
+
+    for (var i = 0; i < this.bills.length; i++) {
+      // Determine percentage based on tipping rules
+      var percentage;
+      //bill selects current position of array
+      var bill = this.bills[i];
+
+      if (bill < 50) {
+        percentage = 0.2;
+      } else if (bill >= 50 && bill < 200) {
+        percentage = 0.15;
+      } else {
+        percentage = 0.1;
+      }
+
+      // Add results to the corresponing arrays
+      this.tips[i] = bill * percentage;
+      this.finalValues[i] = bill + this.tips[i];
+    }
+  }
+};
+
+var mark = {
+  fullName: "Mark Miller",
+  bills: [77, 475, 110, 45],
+  calcTips: function() {
+    this.tips = [];
+    this.finalValues = [];
+
+    for (var i = 0; i < this.bills.length; i++) {
+      // Determine percentage based on tipping rules
+      var percentage;
+      //bill selects current position of array
+      var bill = this.bills[i];
+
+      if (bill < 100) {
+        percentage = 0.2;
+      } else if (bill >= 100 && bill < 300) {
+        percentage = 0.1;
+      } else {
+        percentage = 0.25;
+      }
+
+      // Add results to the corresponing arrays
+      this.tips[i] = bill * percentage;
+      this.finalValues[i] = bill + this.tips[i];
+    }
+  }
+};
+
+function calcAverage(tips) {
+  var sum = 0;
+  for (var i = 0; i < tips.length; i++) {
+    // 0 if sum is 0 = 0 + tips position
+    sum = sum + tips[i];
+    // this will return all the tips added together
+  }
+  //then sum divided by how many in array
+  return sum / tips.length;
+}
+
+//run methods
+
+mark.calcTips();
+john.calcTips();
+
+//do the calculations
+
+//creates average value in object
+// the tips array put through the calc average function
+john.average = calcAverage(john.tips);
+mark.average = calcAverage(mark.tips);
+
+//log john & mark object
+
+console.log(john, mark); // now includes average value
+
+//log who pays higher tips
+
+if (john.average > mark.average) {
+  console.log(
+    john.fullName +
+      "'s family pays higher tips than " +
+      mark.fullName +
+      "'s family"
+  );
+} else {
+  console.log(
+    mark.fullName +
+      "'s family pays higher tips than " +
+      john.fullName +
+      "'s family"
+  );
+}
