4e4d81a043ad106c89d3de4c74f5bb5694de309f
Maxwell Kirwin <maxwellkirwin@Maxwells-iMac.home>
2019-04-30 21:13:07 +0100

    changes

--- app.js
+++ app.js
@@ -11,3 +11,8 @@ console.log(dice);
 //type coericion will change the id tag between 1 and 0 depending on the activePlayer
 document.querySelector("#current-" + activeplayer).textContent = dice;
 //changes the elements textContent so the text content of #current- is now the value of var dice
+
+var x = document.querySelector("#score-0").textContent;
+console.log(x);
+
+document.querySelector(".dice").style.display = "none";
