7533228f229fcd370a589e4c445b356fdddf8be7
max-work-mac <max@dirty-martini.com>
2019-05-14 08:56:21 +0100

    84 done

--- script.js
+++ script.js
@@ -114,6 +114,22 @@ var UIController = (function() {
       document.querySelector(element).insertAdjacentHTML("beforeend", newHtml);
     },
 
+    //CLEAR FIELDS METHOD
+    clearFields: function() {
+      var fields, fieldsArr;
+      fields = document.querySelectorAll(
+        DOMStrings.inputDesc + "," + DOMStrings.inputValue
+      );
+      // turn fields into an array
+      fieldsArr = Array.prototype.slice.call(fields);
+
+      fieldsArr.forEach(function(current, index, array) {
+        current.value = "";
+      });
+      // after fields are cleared focus back to the desc input box
+      fieldsArr[0].focus();
+    },
+
     //Making DOM strings accessible to the public scope
     getDOMStrings: function() {
       return DOMStrings;
@@ -153,7 +169,12 @@ var controller = (function(budgetCtrl, UICtrl) {
     newItem = budgetCtrl.addItem(input.type, input.description, input.value);
     //3. Add the item to the UI
     UICtrl.addlistItem(newItem, input.type);
+
+    //4. Clear the fields
+
+    UICtrl.clearFields();
     //4. Calculate the budget
+
     //5. Display the budget on the UI
   };
 
