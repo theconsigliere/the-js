cf21bddd5b3387f78cb8a4428cd48c2f87c6417f
Max Home <maxwell_kirwin@yahoo.co.uk>
2019-06-09 18:52:27 +0100

    141

--- forkify/src/js/index.js
+++ forkify/src/js/index.js
@@ -1,5 +1,40 @@
 //Global app controller
 
-import x from "./test";
+//import npm package from node modules
+import Search from "./models/search";
 
-console.log(`imported ${x} from another browser via test.js`);
+// Global state of the app
+// * - Search Object
+// * - Current Recipe Object
+// * - Shopping List object
+// * - Liked Recipes
+
+const state = {};
+
+const controlSearch = async () => {
+  // 1) get query from the view
+  const query = "pizza"; // todo
+
+  //2) create new search object
+
+  if (query) {
+    //2) create new search object and add to state
+    //call search.js object constructor
+    state.search = new Search(query);
+
+    // 3) prepare UI for results
+
+    // 4) search for recipes
+    await state.search.getResults();
+
+    // 5) render results on the UI
+    console.log(state.search.result);
+  }
+};
+
+// on submision of search form
+document.querySelector(".search").addEventListener("submit", cur => {
+  //using preventdefault to stop the submission of the form taking you to a new page
+  cur.preventDefault();
+  controlSearch();
+});
---
+++ forkify/src/js/models/search.js
@@ -0,0 +1,24 @@
+//import npm package from node modules
+import axios from "axios";
+
+export default class Search {
+  constructor(query) {
+    this.query = query;
+  }
+
+  // Use async as a method of construtor
+  async getResults() {
+    const key = "30f641ec643488f12ec63459ad7ee18c";
+
+    try {
+      //axios is like 'fetch' for all browsers but automatic returns json
+      const res = await axios(
+        `https://www.food2fork.com/api/search?key=${key}&q=${this.query}`
+      );
+      this.result = res.data.recipes;
+      //   console.log(this.result);
+    } catch (error) {
+      alert(error);
+    }
+  }
+}
