f62a25110bce02ad3d8a9e8da81d2b25a5d7a3bd
max-work-mac <max@dirty-martini.com>
2019-06-10 17:54:20 +0100

    141 1/2

--- forkify/src/js/views/searchView.js
+++ forkify/src/js/views/searchView.js
@@ -7,7 +7,21 @@ import { elements } from "./base";
 export const getInput = () => elements.searchInput.value;
 
 //private function
-const renderRecipe = recipe => {};
+const renderRecipe = recipe => {
+  const markup = `
+  <li>
+    <a class="results__link results__link--active" href="#23456">
+        <figure class="results__fig">
+            <img src="img/test-1.jpg" alt="Test">
+        </figure>
+        <div class="results__data">
+            <h4 class="results__name">Pasta with Tomato ...</h4>
+            <p class="results__author">The Pioneer Woman</p>
+        </div>
+    </a>
+</li>
+ `;
+};
 
 // we return an array of 30 recipes now we loop through these to print ot user interface
 export const renderResults = recipes => {
