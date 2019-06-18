d2ba8a80c81d1eebd2e5c7a80aabe599ac8e4f73
Max Home <maxwell_kirwin@yahoo.co.uk>
2019-06-18 21:04:51 +0100

    node 3

--- node/index.js
+++ node/index.js
@@ -2,6 +2,7 @@
 
 const fs = require("fs");
 const http = require("http");
+const url = require("url");
 
 // RETRIEVING DATA FROM JSON AND CREATING A JAVASCRIPT OBJECT FROM IT
 //-------------------------------------------------------------------------------------------
@@ -9,16 +10,23 @@ const http = require("http");
 //method 'readFileSync' of fs
 // '__dirname' is basically the home folder
 const json = fs.readFileSync(`${__dirname}/data/data.json`, "utf-8");
+
+// SERVER
+//-------------------------------------------------------------------------------------------
+
 const laptopData = JSON.parse(json);
 
 const server = http.createServer((request, response) => {
+  //
+  url.parse(request.url, true);
+
   //let browser know what kind of data we are sending in
   response.writeHead(200, { "Content-type": "text/html" });
   //
   response.end("This is the response");
 });
 
-console.log(__dirname);
+//console.log(__dirname);
 // /Users/maxwellkirwin/Desktop/websites/the-js/node/data
 
 //take 'server' we created add listen at a certain port and address
