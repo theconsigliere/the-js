55030dc43260ad7c8066ec9b0629d9bd0e90a5aa
max-work-mac <max@dirty-martini.com>
2019-06-19 09:25:39 +0100

    165 1/2

--- node/index.js
+++ node/index.js
@@ -11,20 +11,49 @@ const url = require("url");
 // '__dirname' is basically the home folder
 const json = fs.readFileSync(`${__dirname}/data/data.json`, "utf-8");
 
-// SERVER
+// SERVER ROUTING
 //-------------------------------------------------------------------------------------------
 
 const laptopData = JSON.parse(json);
 
 const server = http.createServer((request, response) => {
   //
-  url.parse(request.url, true);
+  const pathName = url.parse(request.url, true).pathname;
+  const id = url.parse(request.url, true).query.id;
 
-  //let browser know what kind of data we are sending in
-  response.writeHead(200, { "Content-type": "text/html" });
-  //
-  response.end("This is the response");
+  if (pathName === "/products" || pathName === "/") {
+    //let browser know what kind of data we are sending in
+    response.writeHead(200, { "Content-type": "text/html" });
+    response.end("This is the product page");
+    //
+  } else if (pathName === "/laptop" && id < laptopData.length) {
+    response.writeHead(200, { "Content-type": "text/html" });
+
+    //async function
+    fs.readFile(
+      `${__dirname}/templates/template-laptop.html`,
+      "utf-8",
+      (error, data) => {
+        const laptop = laptopData[id];
+        let output = data.replace("{%PRODUCTNAME%}", laptop.productName);
+        let output = output.replace("{%IMAGE%}", laptop.image);
+        let output = output.replace("{%PRICE%}", laptop.price);
+        let output = output.replace("{%SCREEN%}", laptop.screen);
+        let output = output.replace("{%CPU%}", laptop.cpu);
+        let output = output.replace("{%STORAGE%}", laptop.storage);
+        let output = output.replace("{%RAM%}", laptop.ram);
+        let output = output.replace("{%DESCRIPTION%}", laptop.description);
+      }
+    );
+    //
+  } else {
+    response.writeHead(200, { "Content-type": "text/html" });
+    response.end("Url was not found");
+  }
 });
+
+// SERVER LISTEN
+//-------------------------------------------------------------------------------------------
 
 //console.log(__dirname);
 // /Users/maxwellkirwin/Desktop/websites/the-js/node/data
